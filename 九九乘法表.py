a333aa7e686c28020fe351adf764505414639c60
panzervi8 <174860451@qq.com>
2019-10-26 11:12:31 +0800

    九九乘法表

---
+++ 九九乘法表.py
@@ -0,0 +1,11 @@
+'''
+构建一个九九乘法表
+'''
+
+row = 5
+for i in range(row):
+    for _ in range(row - i - 1):
+        print(' ', end='')
+    for _ in range(2 * i + 1):
+        print('*', end='')
+    print()
